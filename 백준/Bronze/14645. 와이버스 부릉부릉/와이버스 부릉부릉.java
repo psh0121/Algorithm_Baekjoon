6cb07904e9b4de86055f6361ccb78ef6455053b2
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-06 07:01:43 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 192 ms, Memory: 17316 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			sc.next();
+			sc.next();
+		}
+		
+		System.out.println("비와이");
+	}
+}         
