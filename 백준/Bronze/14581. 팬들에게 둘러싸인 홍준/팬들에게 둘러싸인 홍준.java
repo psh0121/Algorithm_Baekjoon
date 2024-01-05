d6aa6c141da0acf704ac2bcfdac303b4eb9809ab
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-06 06:58:55 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 232 ms, Memory: 18540 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String id = sc.next();
+		String fan = ":fan:";
+		
+		System.out.println(fan + fan + fan);
+		System.out.println(fan + ":" + id + ":" + fan);
+		System.out.println(fan + fan + fan);
+	}
+}         
