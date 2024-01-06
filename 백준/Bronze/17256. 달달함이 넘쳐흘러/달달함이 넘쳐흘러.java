a9d64333e953dd13130d3a9a0d2d336859ab5155
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-07 07:14:53 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 240 ms, Memory: 18600 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int ax = sc.nextInt();
+		int ay = sc.nextInt();
+		int az = sc.nextInt();
+		
+		int cx = sc.nextInt();
+		int cy = sc.nextInt();
+		int cz = sc.nextInt();
+		
+		int bx, by, bz;
+		
+		bx = cx - az;
+		by = cy / ay;
+		bz = cz - ax;
+		
+		System.out.println(bx + " " + by + " " + bz);
+	}
+}         
