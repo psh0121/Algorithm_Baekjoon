a899a4e58c553da6bf79a1aabe5745cbdda034c7
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-07 07:05:08 +0900

    [Bronze V] Title: 홍익대학교, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int startYear = 1946;
+		int getYear = sc.nextInt();
+		
+		System.out.println(getYear - startYear);
+	}
+}         
