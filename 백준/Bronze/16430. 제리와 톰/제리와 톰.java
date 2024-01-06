3237f366f191b930b7bb11a890821faa3a582075
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-07 07:07:41 +0900

    [Bronze V] Title: 제리와 톰, Time: 232 ms, Memory: 18376 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int top = sc.nextInt();
+		int bottom = sc.nextInt();
+		
+		System.out.print((bottom - top) + " " + bottom);
+	}
+}         
