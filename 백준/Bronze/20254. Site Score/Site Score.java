feaf4dc3a2c6bc59ab81df32e75928d5b0ed27a3
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-07 15:20:48 +0900

    [Bronze V] Title: Site Score, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int ur = sc.nextInt();
+		int tr = sc.nextInt();
+		int uo = sc.nextInt();
+		int to = sc.nextInt();
+		
+		System.out.println((56*ur) + (24*tr) + (14 * uo) + (6 * to));
+	}
+}         
