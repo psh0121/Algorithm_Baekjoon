2bf31ac7a89e5bd9e1a3b2b7d9c4c105606e3114
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-07 23:44:25 +0900

    [Bronze V] Title: 身長 (Height), Time: 208 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int before = sc.nextInt();
+		int after = sc.nextInt();
+		
+		System.out.println(after - before);
+	}
+}         
