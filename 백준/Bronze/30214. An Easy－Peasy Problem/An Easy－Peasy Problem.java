7416891196be3af029b0f6b8c765860eb90fcdc1
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-08 18:45:45 +0900

    [Bronze V] Title: An Easy-Peasy Problem, Time: 208 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/30214. An Easy－Peasy Problem/An Easy－Peasy Problem.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int s1 = sc.nextInt();
+		int s2 = sc.nextInt();
+		
+		if(s1*2 >= s2) System.out.println("E");
+		else System.out.println("H");
+	}
+}         
