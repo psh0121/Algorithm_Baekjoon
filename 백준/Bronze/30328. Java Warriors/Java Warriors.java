154ccad002c005aab9f5f7899e495f4e3472ef2e
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-08 18:09:16 +0900

    [Bronze V] Title: Java Warriors, Time: 200 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int cnt = sc.nextInt();
+		
+		System.out.println(4000 * cnt);
+	}
+}         
