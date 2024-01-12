eaf918e487d6ac0d9b92357ff245d25e5edc7701
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-13 06:41:11 +0900

    [Bronze IV] Title: Next in line, Time: 204 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int age1 = sc.nextInt();
+		int age2 = sc.nextInt();
+		
+		System.out.println(age2 + (age2 - age1));
+	}
+}         
