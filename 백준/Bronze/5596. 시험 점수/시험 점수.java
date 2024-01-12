b4fc4ccaa22369c56ce80ca45634c54e2f76d1de
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-13 06:25:07 +0900

    [Bronze IV] Title: 시험 점수, Time: 200 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/5596. 시험 점수/시험 점수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int s = 0;
+		int t = 0;
+		
+		for(int i = 0; i < 4; i++) s += sc.nextInt();
+		for(int i = 0; i < 4; i++) t += sc.nextInt();
+		
+		System.out.println(Math.max(s, t));
+	}
+}         
