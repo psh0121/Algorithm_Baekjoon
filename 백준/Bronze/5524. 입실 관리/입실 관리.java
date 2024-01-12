8e9e4894059a0c84eae5447f7030004ac440dbee
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-13 00:03:49 +0900

    [Bronze IV] Title: 입실 관리, Time: 220 ms, Memory: 17732 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int cnt = sc.nextInt();
+		
+		for(int i = 0; i < cnt; i++) {
+			System.out.println(sc.next().toLowerCase());
+		}
+		
+	}
+}         
