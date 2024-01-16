8c72e7f8a27729a2f35fc736c6a4a252c3b0e1c3
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-16 10:00:59 +0900

    [Bronze IV] Title: Quadrants, Time: 220 ms, Memory: 17996 KB -BaekjoonHub

---
+++ 백준/Bronze/9772. Quadrants/Quadrants.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		double x, y;
+		
+		while(sc.hasNext()) {
+			x = sc.nextDouble();
+			y = sc.nextDouble();
+			
+			if(x == 0 || y == 0) System.out.println("AXIS");
+			else {
+				if(x > 0 && y > 0) System.out.println("Q1");
+				else if(x < 0 && y > 0) System.out.println("Q2");
+				else if(x < 0 && y < 0) System.out.println("Q3");
+				else System.out.println("Q4");
+			}
+		}
+	}
+}         
