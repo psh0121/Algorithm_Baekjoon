b729bb9331041e7b9dcf1537fd73460ddb8e4350
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-17 12:53:10 +0900

    [Bronze IV] Title: 한글 2, Time: 208 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		char val = sc.next().charAt(0);
+		
+		System.out.println((int)val - 44031);
+	}
+}         
