bc9993660104f3fe82eddaca4c9b08d71e7ea3c1
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-19 09:39:20 +0900

    [Bronze IV] Title: Tournament Selection, Time: 196 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Bronze/14038. Tournament Selection/Tournament Selection.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int cnt = 6;
+		int winCnt = 0;
+		
+		for(int i = 0; i < cnt; i++) {
+			if(sc.next().equals("W")) winCnt++;
+		}
+		
+		if(winCnt >= 5) System.out.println(1);
+		else if(winCnt >= 3) System.out.println(2);
+		else if(winCnt >= 1) System.out.println(3);
+		else System.out.println(-1);
+		
+		sc.close();
+	}
+}         
