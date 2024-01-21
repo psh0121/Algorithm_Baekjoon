727c4f079b9c6fad4c970f6e03a9c1e1ce5f4171
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-21 10:02:12 +0900

    [Bronze IV] Title: A Simple Problem., Time: 2088 ms, Memory: 123304 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int cnt = Integer.parseInt(br.readLine());
+		BigInteger num;
+		
+		for(int i = 0; i < cnt; i++) {
+			num = new BigInteger(br.readLine());
+			
+			System.out.println(num.multiply(num));
+		}
+	}
+}         
