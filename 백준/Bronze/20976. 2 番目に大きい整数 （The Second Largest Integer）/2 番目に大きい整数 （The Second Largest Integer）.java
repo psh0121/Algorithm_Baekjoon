cecf3a4756aee60eecaf399f4447e96de72ad757
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-25 06:38:07 +0900

    [Bronze IV] Title: 2 番目に大きい整数 (The Second Largest Integer), Time: 212 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/20976. 2 番目に大きい整数 （The Second Largest Integer）/2 番目に大きい整数 （The Second Largest Integer）.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[] {sc.nextInt(), sc.nextInt(), sc.nextInt()};
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[1]);
+	}
+}         
