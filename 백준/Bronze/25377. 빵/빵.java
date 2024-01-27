f59232f24b2ccdbe0ea120919745ca263b7582b5
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-01-28 06:59:04 +0900

    [Bronze IV] Title: 빵, Time: 128 ms, Memory: 14272 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.java
+++ 백준/Bronze/25377. 빵/빵.java
@@ -1,24 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int min = Integer.MAX_VALUE;
+		int cnt = Integer.parseInt(br.readLine());
 		
-		int result = -1;
-		
-		for(int i = 0; i < n; i++) {
-			int walkTime = sc.nextInt();
-			int breadTime = sc.nextInt();
+		for(int i = 0; i < cnt; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int walkT = Integer.parseInt(st.nextToken());
+			int breadT = Integer.parseInt(st.nextToken());
 			
-			if(walkTime > breadTime) continue;
-			
-			if(i == 0) result = breadTime;
-			else result = Math.min(result, breadTime);
+			if(walkT > breadT) continue;
+			min = Math.min(min, breadT);
 		}
 		
-		System.out.println(result);
+		System.out.println(min == Integer.MAX_VALUE ? -1 : min);
+		
+		br.close();
 	}
 }         
