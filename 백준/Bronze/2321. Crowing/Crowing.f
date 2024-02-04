9404db9a017597fb9605181238042eef9e0c69f0
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-02-04 09:16:03 +0900

    [Bronze III] Title: Crowing, Time: 0 ms, Memory: 1408 KB -BaekjoonHub

---
+++ 백준/Bronze/2321. Crowing/Crowing.f
@@ -0,0 +1,8 @@
+program Q2321
+    PRINT '(a)', ",~"
+    PRINT '(a)', "( 9> //)"
+    PRINT '(a)', " )(_///"
+    PRINT '(a)', "( \_>/"
+    PRINT '(a)', " \__/"
+    PRINT '(a)', "  ^^"
+end program Q2321
