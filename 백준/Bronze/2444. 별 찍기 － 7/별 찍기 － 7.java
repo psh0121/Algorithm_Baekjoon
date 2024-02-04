901a05cb046a4bef34d8625e983cf5daa0ca11f9
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-02-05 06:37:43 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 224 ms, Memory: 17880 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int space = n - 1;
+		
+		for(int i = 1; i <= n; i++) {
+			System.out.println(" ".repeat(space--) + "*".repeat(2*i-1));
+		}
+		
+		space = 1;
+		
+		for(int i = n-1; i >= 1; i--) {
+			System.out.println(" ".repeat(space++) + "*".repeat(2*i-1));
+		}
+	}
+}
+
