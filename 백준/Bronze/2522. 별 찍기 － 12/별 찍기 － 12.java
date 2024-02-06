0261ef3ec39234f5c85e530414c81f390c4aede3
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-02-06 13:41:49 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 224 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			System.out.println(" ".repeat(n-i) + "*".repeat(i));
+		}
+		
+		for(int i = n-1; i >= 1; i--) {
+			System.out.println(" ".repeat(n-i) + "*".repeat(i));
+		}
+	}
+}
+
