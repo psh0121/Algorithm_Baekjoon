54efa520f22fb769d8a851afdd3d6c1b80e486f8
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-03-14 15:31:43 +0900

    [Bronze IV] Title: 和の判定 (Sum Checker), Time: 204 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/31607. 和の判定 （Sum Checker）/和の判定 （Sum Checker）.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	
+    public static void main(String[] args){
+    	
+    	Scanner sc = new Scanner(System.in);
+        int[] arr = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
+        Arrays.sort(arr);
+        
+        System.out.println(arr[0] + arr[1] == arr[2] ? 1 : 0);
+    }
+}
