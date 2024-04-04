35f6ac35206c4fd1e105c8eb1dbe321d195b8962
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-04-04 14:08:16 +0900

    [Bronze III] Title: Counting Swann’s Coins, Time: 224 ms, Memory: 18296 KB -BaekjoonHub

---
+++ 백준/Bronze/5292. Counting Swann’s Coins/Counting Swann’s Coins.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	
+    public static void main(String[] args){
+    	
+    	Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	
+    	for(int i = 1; i <= n; i++) {
+    		if(i % 15 == 0) System.out.println("DeadMan");
+    		else if(i % 3 == 0) System.out.println("Dead");
+    		else if(i % 5 == 0) System.out.println("Man");
+    		else System.out.print(i + " ");
+    	}
+    	sc.close();
+    }
+}
