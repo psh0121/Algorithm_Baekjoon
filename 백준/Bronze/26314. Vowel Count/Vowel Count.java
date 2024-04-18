e66d60552fde39af84c1f87208963a016a1afab4
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-04-18 17:06:14 +0900

    [Bronze III] Title: Vowel Count, Time: 208 ms, Memory: 17436 KB -BaekjoonHub

--- 백준/Bronze/26314. Vowel Count/Vowel Count.java
+++ 백준/Bronze/26314. Vowel Count/Vowel Count.java
@@ -5,24 +5,23 @@ public class Main {
     public static void main(String[] args){
     	
     	Scanner sc = new Scanner(System.in);
-    	
     	int n = sc.nextInt();
     	String aeiou = "aeiou";
     	
     	for(int i = 0; i < n; i++) {
     		String str = sc.next();
-    		int aeiouCnt = 0;
+    		int cnt = 0;
     		
     		for(int j = 0; j < str.length(); j++) {
-    			if(aeiou.contains(String.valueOf(str.charAt(j)))) {
-    				aeiouCnt++;
-    			}
+    			if(aeiou.contains(Character.toString(str.charAt(j)))) cnt++;
     		}
     		
     		System.out.println(str);
-    		if(aeiouCnt > (str.length() - aeiouCnt)) System.out.println(1);
-    		else System.out.println(0);
+    		
+    		if(str.length()-cnt >= cnt) System.out.println(0);
+    		else System.out.println(1);
     	}
+    	
     	sc.close();
     }
 }
