77417af4bcdb6393dfa7d870853d945709cd3784
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-05-17 17:38:57 +0900

    [Bronze III] Title: 행운의 편지, Time: 112 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/13311. 행운의 편지/행운의 편지.java
@@ -0,0 +1,13 @@
+//import java.util.Scanner;
+
+public class Main {
+	
+    public static void main(String[] args) {
+    	//Scanner sc = new Scanner(System.in);
+    	
+    	// 2부터 1000까지의 정수를 어떤 수로 나눴을 떄, 나머지가 모두 같은 것 
+    	
+    	System.out.println(-1);
+    	//sc.close();
+    }
+}
