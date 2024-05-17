118ca83b2f2529f944f8058968390b01871cefe9
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-05-17 16:18:24 +0900

    [Bronze III] Title: 삼각 무늬 - 1, Time: 224 ms, Memory: 17876 KB -BaekjoonHub

---
+++ 백준/Bronze/11320. 삼각 무늬 － 1/삼각 무늬 － 1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int t = sc.nextInt();	// testcase
+    	
+    	for(int i = 0; i < t; i++) {
+    		int a = sc.nextInt();
+        	int b = sc.nextInt();
+        	
+        	System.out.println((int)Math.pow(a / b, 2));
+    	}
+    	
+    	
+    }
+}
