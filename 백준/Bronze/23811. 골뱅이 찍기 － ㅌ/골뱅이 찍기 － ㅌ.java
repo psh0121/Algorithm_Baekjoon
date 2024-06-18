38419318bebdc6a5be1ba786baa92123deff4870
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-06-18 17:25:18 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 204 ms, Memory: 17828 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+    	Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for(int j = 0; j < 2; j++) {
+			for(int i = 0; i < n; i++) {
+				System.out.println("@".repeat(n*5));
+			}
+	
+			for(int i = 0; i < n; i++) {
+				System.out.println("@".repeat(n));
+			}
+		}
+
+		for(int i = 0; i < n; i++) {
+			System.out.println("@".repeat(n*5));
+		}
+		
+		sc.close();
+    }
+}
