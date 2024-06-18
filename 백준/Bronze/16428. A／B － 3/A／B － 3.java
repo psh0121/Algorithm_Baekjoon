0ec7c39ffb01e9889ee60804c134061a691688d6
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-06-18 16:16:37 +0900

    [Bronze III] Title: A/B - 3, Time: 412 ms, Memory: 20984 KB, Score: 630 point -BaekjoonHub

--- 백준/Bronze/16428. A／B － 3/A／B － 3.java
+++ 백준/Bronze/16428. A／B － 3/A／B － 3.java
@@ -7,8 +7,21 @@ public class Main {
 		BigInteger a = sc.nextBigInteger();
 		BigInteger b = sc.nextBigInteger();
 
-		System.out.println(a.divide(b));
-		System.out.println(a.remainder(b));
+		int compareA0 = a.compareTo(new BigInteger("0"));
+		int compareB0 = b.compareTo(new BigInteger("0"));
+
+		if(compareA0 == -1 && compareB0 >= 0) {
+			System.out.println(a.divide(b).subtract(new BigInteger("1")));
+			System.out.println(a.remainder(b).add(b));
+		}
+		else if (compareA0 == -1 && compareB0 == -1) {
+			System.out.println(a.divide(b).add(new BigInteger("1")));
+			System.out.println(a.remainder(b).subtract(b));
+		}
+		else {
+			System.out.println(a.divide(b));
+			System.out.println(a.remainder(b));
+		}
 		sc.close();
     }
 }
