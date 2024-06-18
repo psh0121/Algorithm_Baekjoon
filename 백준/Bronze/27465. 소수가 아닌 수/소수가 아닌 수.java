d24e03c4bf245a563e33198aec49438c75c43e45
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-06-18 14:00:30 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 208 ms, Memory: 17732 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.java
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.java
@@ -1,24 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-
-	public static long nextNotPrime(long n) {
-		long num = n;
-		if(num % 2 == 0) num += 2;
-
-		return num;
-	}
-	
     public static void main(String[] args){
     	Scanner sc = new Scanner(System.in);
-		long n = sc.nextLong();
+		int n = sc.nextInt();
 
-		if(n == 2 || n == 1) {
-			System.out.println(4);
-			return;
-		}
-
-		System.out.println(nextNotPrime(n));
+		System.out.println((int)Math.pow(10, 9));
 		sc.close();
     }
 }
