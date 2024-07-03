957bf19670b24fa9cff34181b1a5c4898a88ccd8
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-07-03 10:27:12 +0900

    [Bronze III] Title: 단원평가, Time: 152 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/31798. 단원평가/단원평가.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		if(a == 0) System.out.println((c * c) - b);
+		else if (b == 0) System.out.println((c * c) - a);
+		else System.out.println((int)Math.sqrt(a + b));
+		sc.close();
+    }
+}
