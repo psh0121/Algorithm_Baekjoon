f0b1586256a5d6d6b24a8ca98f97d9983782efe3
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-09-13 09:31:12 +0900

    [Bronze III] Title: Начало, Time: 172 ms, Memory: 17868 KB -BaekjoonHub

---
+++ 백준/Bronze/29319. Начало/Начало.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+		Arrays.sort(arr);
+
+		int sum = 0;
+		for(int i = 0; i < n-1; i++) sum += arr[i];
+
+		System.out.println(sum);
+    }
+}
