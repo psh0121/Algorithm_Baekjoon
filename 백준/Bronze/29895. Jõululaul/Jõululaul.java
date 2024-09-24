8c97b421797a77162bcbc2619344da970744a473
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-09-25 00:09:49 +0900

    [Bronze III] Title: Jõululaul, Time: 852 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Bronze/29895. Jõululaul/Jõululaul.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		long[] arr = new long[n+1];
+
+		for(int i = 1; i <= n; i++) {
+			arr[i] = (long)i * (n - i + 1);
+		}
+
+		for(int i = 1; i <= n; i++) {
+			System.out.println(arr[i]);
+		}
+		sc.close();
+    }
+}
