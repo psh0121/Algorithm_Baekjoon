b2dddf048197bc844774d14f51b93c80db796fcd
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-10-07 08:43:59 +0900

    [Bronze V] Title: Just Round Down, Time: 176 ms, Memory: 17996 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		double x = sc.nextDouble();
+
+		System.out.println((int)Math.floor(x));
+		sc.close();
+    }
+}
