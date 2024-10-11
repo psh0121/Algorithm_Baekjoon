e580d57d64ead9efb0c301bc448ee17fe26a0a6c
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-10-11 23:42:33 +0900

    [Bronze II] Title: 우유 축제, Time: 216 ms, Memory: 20080 KB -BaekjoonHub

--- 백준/Bronze/14720. 우유 축제/우유 축제.java
+++ 백준/Bronze/14720. 우유 축제/우유 축제.java
@@ -1,38 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
-	
-    public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	int n = sc.nextInt();
-    	int drinkCnt = 0;	// 최대마실수있는 우유 개수
-    	int beforeMilk = -1;	// 이전에 마신 우유 
-    	
-    	for(int i = 0; i < n; i++) {
-    		int milk = sc.nextInt();
-    		
-    		// 첫시작 
-    		if(beforeMilk == -1 && milk == 0) {
-    			beforeMilk = milk;
-    			drinkCnt++;
-    			continue;
-    		}
-    		
-    		if(beforeMilk == 0 && milk == 1) {
-    			beforeMilk = milk;
-    			drinkCnt++;
-    		}
-    		else if(beforeMilk == 1 && milk == 2) {
-    			beforeMilk = milk;
-    			drinkCnt++;
-    		}
-    		else if(beforeMilk == 2 && milk == 0) {
-    			beforeMilk = milk;
-    			drinkCnt++;
-    		}
-    	}
-    	
-    	System.out.println(drinkCnt);
-    	sc.close();
+    public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int milk = 0;
+		int cnt = 0;
+
+		for(int i = 0; i < n; i++) {
+			int store = sc.nextInt();
+
+			if(store == milk) {
+				if(milk == 0) {
+					cnt++;
+					milk = 1;
+				}
+				else if (milk == 1) {
+					cnt++;
+					milk = 2;
+				}
+				else if (milk == 2) {
+					cnt++;
+					milk = 0;
+				}
+			}
+		}
+		System.out.println(cnt);
+		sc.close();
     }
 }
