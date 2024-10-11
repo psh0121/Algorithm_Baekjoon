f4d12eed926b6e42792e434d100af38f58e32d4e
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-10-12 06:41:00 +0900

    [Bronze II] Title: 행복, Time: 108 ms, Memory: 14496 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15969. 행복/행복.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		String[] strArr = br.readLine().split(" ");
+		int[] arr = new int[n];
+
+		for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(strArr[i]);
+
+		Arrays.sort(arr);
+
+		System.out.println(arr[n-1] - arr[0]);
+    }
+}
