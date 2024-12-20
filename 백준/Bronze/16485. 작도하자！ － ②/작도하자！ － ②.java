3d3b0dc5d8202627ea0daa99c7c22921c39182e1
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2024-12-20 19:25:47 +0900

    [Bronze II] Title: 작도하자! - ②, Time: 184 ms, Memory: 17932 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Bronze/16485. 작도하자！ － ②/작도하자！ － ②.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+    double c = sc.nextDouble();
+    double b = sc.nextDouble();
+
+    System.out.println(c/b);
+		sc.close();
+    }
+}
