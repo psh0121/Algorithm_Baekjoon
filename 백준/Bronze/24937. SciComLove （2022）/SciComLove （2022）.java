0e22575afd474e89215a40ab576f0a18c464db04
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2025-01-06 20:19:32 +0900

    [Bronze II] Title: SciComLove (2022), Time: 176 ms, Memory: 17748 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/24937. SciComLove （2022）/SciComLove （2022）.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("SciComLove".split("")));
+
+        for(int i = 0; i < n; i++) {
+          list.add(list.remove(0));
+        }
+
+        String result = "";
+
+        for(String letter : list) {
+          result += letter;
+        }
+
+        System.out.println(result);
+        sc.close();
+    }
+}
