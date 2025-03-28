a92018bc58383c405c39f968c5b810b46d4ba8d2
Suhyeon Park <65241463+psh0121@users.noreply.github.com>
2025-03-29 07:02:35 +0900

    [Bronze II] Title: 箱と鍵 (Boxes and Keys), Time: 184 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Bronze/24081. 箱と鍵 （Boxes and Keys）/箱と鍵 （Boxes and Keys）.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        int[] a = new int[n];
+        int[] b = new int[m];
+        for(int i = 0; i < n; i++) a[i] = sc.nextInt();
+        for(int i = 0; i < m; i++) b[i] = sc.nextInt();
+
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(a[i] == b[j]) {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+        sc.close();
+    }
+}
